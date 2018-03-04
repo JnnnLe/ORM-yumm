dd10038d64a3f5289f682088710a35f25ee19914
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-03-04 09:20:46 -0800

    Established / route for getAll method

---
+++ controllers/burgersController.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+
+  burger.all(function (data) {
+    var hbsObj = {
+      burgers: data
+    };
+
+    console.log(hbsObj);
+    res.render("index", "hbsObj");
+  });
+});
+
+module.exports = router;
+// Update schema for boolean value once burgur has been devoured?
