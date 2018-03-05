85a1a894c4e18fc6591bf8e2dc5cf1e90c23c6f5
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-03-04 23:58:04 -0800

    Implement button function for ajax requests

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+$(function() {
+  $(".change-de").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEat = $(this).data("newEat");
+    var newEatState = {
+      isDevoured: newEat
+  };
+  location.reload()
+
+  $.ajax("/api/burgers/" + id, {
+    type: "PUT",
+    data: newEatState
+  }).then(
+    function() {
+      console.log("Changed devoured to ", newEat);
+      // location.reload();
+    }
+  );
+});
+
+$(".create-form").on("submit", function(event) {
+  event.preventDefault();
+  var newBurger = {
+    burger_name: $("#bu").val().trim(),
+    isDevoured: $("[name=name]").val().trim()
+  }
+  $.ajax("/api/burgers", {
+    type: "POST",
+    data: newBurger
+  }).then(
+    function() {
+      console.log("Created new burger!");
+      location.reload()
+    }
+  )
+})
+
+
+
+
+
+
+  // Closes out entire function.
+});
