3c577ede126f91731e7a31d41712b648e6f2be79
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-03-04 23:25:54 -0800

    Implement endpoints for express router

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,21 +1,33 @@
 var express = require("express");
+var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
-
 router.get("/", function(req, res) {
-
   burger.all(function(data) {
     var hbsObj = {
       burgers: data
     };
-
-    console.log(hbsObj);
     res.render("index", hbsObj);
   });
 });
 
+router.post("/api/burgers/", function(req, res) {
+  burger.createOne([
+    "burger_name"], [req.body.burger_name],
+    function(data) {
+    res.redirect("/")
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("Condition: ", condition)
+  burger.updateOne({
+    isDevoured: true
+  }, condition, function(data) {
+    res.redirect("/");
+  });
+});
+
 module.exports = router;
-// Update schema for boolean value once burgur has been devoured?
