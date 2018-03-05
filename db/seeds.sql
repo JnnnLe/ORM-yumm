770acd25b372a705a1ce7bd0af4f62ec9bfa7ef4
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-03-05 01:01:01 -0800

    Add scroll feat to burger list

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO burgers (burger_name)
-VALUES ("Jerome C. Savage burger"), ("Spicy chicken burger"), ("Pineapple Terryaki Burger"), ("Double Bacon Cheeseburger");
+VALUES ("Jerome C. Savage burger"), ("Spicy chicken burger"), ("Pineapple Terryaki Burger"), ("Double Bacon Cheeseburger"), ("Life's all Good burger"), ("Jenn Jenn burger"), ("Max spicy burger"), ("Share w/ Your Sweetie burger");
