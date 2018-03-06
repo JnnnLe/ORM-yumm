8259bf5ef5d6cf59dfbe91c56515d1df23c6ce72
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-03-05 16:08:51 -0800

    Modify dummy data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO burgers (burger_name)
-VALUES ("Jerome C. Savage burger"), ("Spicy chicken burger"), ("Pineapple Terryaki Burger"), ("Double Bacon Cheeseburger"), ("Life's all Good burger"), ("Jenn Jenn burger"), ("Max spicy burger"), ("Share w/ Your Sweetie burger");
+VALUES ("Jerome C. Savage burger"), ("AmBurger"), ("Honest burger"), ("Life's all Good burger"), ("Jenn Jenn burger"), ("Max spicy burger"), ("Share w/ Your Sweetie burger"), ("Extra Sash burger");
