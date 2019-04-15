36132de66532ffab481083083d4bbe5a351327db
Jennifer Lê <jnnnle.sf@gmail.com>
2019-04-15 11:40:12 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO burgers (burger_name)
-VALUES ("Jerome C. Savage burger"), ("AmBurger"), ("Honest burger"), ("Life's all Good burger"), ("Jenn Jenn burger"), ("Max spicy burger"), ("Share w/ Your Sweetie burger"), ("Extra Sash burger");
+VALUES  ("Honest burger"), ("Life's all Good burger"), ("Jenn Jenn burger"), ("Max spicy burger"), ("Share w/ Your Sweetie burger"), ("Extra Sash burger");
